4fe060e8a319d1b7dcc6eb23ecf4e5ef7d97d014
Brad Cassity <bradley.cassity@gmail.com>
2021-10-19 08:45:54 -0600

    Added basic project structure. Created server.js file. Included nodemailer and express to package.json file.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Routes
+// app.use('/', routes);
+
+// start server on selected port
+const PORT = process.env.API_PORT || '3001';
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
